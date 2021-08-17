71635f9dc79c82a91e312a5206526c779ee9476a
JellyPork <55303387+JellyPork@users.noreply.github.com>
2021-08-17 10:50:34 -0700

    update
    meh

--- src/test/java/RentaAuto.java
+++ src/test/java/RentaAuto.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,7 +13,12 @@ public class RentaAuto {
     
     private String cad;//Ciudad a Devolver
     private String car;//Ciudad a Recoger
+    private String fechaDevolver; 
+    private String fechaRecoger;
     private String tipoAuto; //Tipo de auto
+    private boolean seRento; //Verificar si la renta si fue aceptada
+    private int idRenta; //id que puede ser usado para buscar la renta
+    private float costoRenta; 
     
     
 }
